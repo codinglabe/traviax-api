a053ae176a1628cfba90144a7bfad8dfd51bf98e
codinglabe <93982862+codinglabe@users.noreply.github.com>
2022-03-22 00:30:02 +0600

    Migration make
    Migration make

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string("main_job");
+            $table->string("postal_code");
+            $table->string("telephone");
+            $table->string("name_or_siret_num")->nullable();
+            $table->string("company_name");
+            $table->string("address");
+            $table->string("architect");
+            $table->string("distance_km");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
